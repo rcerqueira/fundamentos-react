9551420094657c35447af283612ea106d4766090
Ricardo Cerqueira <rcerqueira@gmail.com>
2020-04-26 20:43:04 -0300

    Adicionado sinal de negativo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -91,7 +91,7 @@ const Dashboard: React.FC = () => {
                 <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
                   <td className={transaction.type}>
-                    {transaction.type == 'outcome' ? '- ' : ''}
+                    {transaction.type === 'outcome' ? '- ' : ''}
                     {formatValue(transaction.value)}
                   </td>
                   <td>{transaction.category.title}</td>
